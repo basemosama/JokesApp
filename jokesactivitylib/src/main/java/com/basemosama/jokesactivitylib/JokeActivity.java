cbdddce5b0e98acde868397265bb0362345b89a9
Basem Osama <engbasemosama@gmail.com>
2019-02-18 20:44:00 +0200

    Fixing issue with joke button id

--- jokesactivitylib/src/main/java/com/basemosama/jokesactivitylib/JokeActivity.java
+++ jokesactivitylib/src/main/java/com/basemosama/jokesactivitylib/JokeActivity.java
@@ -1,5 +1,6 @@
 package com.basemosama.jokesactivitylib;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -9,15 +10,21 @@ import com.basemosama.jokesactivitylib.R;
 
 public class JokeActivity extends AppCompatActivity {
 
+    private String joke="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
 
         TextView jokeTv=findViewById(R.id.joke_tv);
-      String joke = getIntent().getStringExtra(getString(R.string.joke_key));
 
-      if( joke.isEmpty() || joke.equals(getString(R.string.default_error_message))) {
+        Intent intent=getIntent();
+        if (intent.hasExtra(getString(R.string.joke_key)))
+            joke = getIntent().getStringExtra(getString(R.string.joke_key));
+
+
+        if( joke.isEmpty() || joke.equals(getString(R.string.default_error_message))) {
           Toast.makeText(this,R.string.error_message,Toast.LENGTH_SHORT).show();
       }else {
           jokeTv.setText(joke);
